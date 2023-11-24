50a99f336506c3cc059ead90b88ec7bbc528dd21
Grace Do <61570746+grace1501@users.noreply.github.com>
2023-11-24 13:13:46 -0500

    1st week calculation

--- script.js
+++ script.js
@@ -5,12 +5,30 @@
 // The area is starting with 20 plants.
 // The plants double in number every week.
 
-const areaRadius = 5;
 const PI = 3.1415;
-const area = PI * radius * radius;
+// const area = PI * radius * radius;
 
-const plantRadius = 0.8;
+const gardenRadius = 5;
+const totalGardenArea = PI * gardenRadius * gardenRadius;
+
+const plantSpace = 0.8;
+
 const startPlantNumber = 20;
+let plantNumberAfterNWeek;
+
+let prunedDecision = "Let's prune the plants, they are exceeding the capacity of the garden."
+let monitoredDecision = "It's ok for now, the plants are growing at an acceptable rate."
+let plantedDecision = "There is room to plant more plants."
 
 // Result AFTER 1 WEEK
-let plantNumberWeek1 = startPlantNumber*2;
+plantNumberAfterNWeek = startPlantNumber*2;
+console.log(`After 1 week, the total plant number is: ${plantNumberAfterNWeek}`)
+let growingRatio = (plantNumberAfterNWeek * plantSpace) / totalGardenArea;
+
+if ( growingRatio < 0.5) {
+    console.log(plantedDecision);
+} else if (growingRatio <= 0.8) {
+    console.log(monitoredDecision)
+} else {
+    console.log(prunedDecision);
+}
